6c08e9fcf684eff60ead23dee26c475d917b2b42
sobhan.ahmadian.moghadam <sobhan.ahmadian.moghadam@gmail.com>
2024-05-11 16:39:37 +0330

    Update model.py. Add docstrings. add _build_model and _build_feature_extractor functions to ModelHandler.

--- src/soma_integ/model.py
+++ src/soma_integ/model.py
@@ -1,30 +1,70 @@
 import abc
 
+from .config import ModelConfig
 from .utils import logging as base_logger
 
 logger = base_logger.getLogger(__name__)
 
 
 class ModelHandler(abc.ABC):
-    def __init__(self, model_config) -> None:
+    """
+    Abstract base class for model handlers.
+
+    Args:
+        model_config (ModelConfig): Configuration of the model.
+    """
+
+    def __init__(self, model_config: ModelConfig) -> None:
         self.model_config = model_config
-        self.model = None
+        self.model = self._build_model()
+        self.fe = self._build_feature_extractor()
 
     @abc.abstractmethod
     def destroy(self):
-        logger.info(f'Model {self.model} has been deleted')
+        """
+        Destroys the model.
+        """
+        logger.info(f"Model {self.model} has been deleted.")
         raise NotImplementedError
 
     @abc.abstractmethod
     def predict(self, **kwargs):
+        """
+        Makes predictions using the model.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def summary(self):
+        """
+        Prints a summary of the model.
+        """
+        raise NotImplementedError
+
+    @abs.abstractmethod
+    def _build_model(self):
+        """
+        Builds the model.
+        """
+        raise NotImplementedError
+
+    @abs.abstractmethod
+    def _build_feature_extractor(self):
+        """
+        Builds the feature extractor.
+        """
         raise NotImplementedError
 
 
 class HandlerFactory(abc.ABC):
+    """Abstract base class for handler factories."""
+
     @abc.abstractmethod
     def create_handler(self) -> ModelHandler:
+        """
+        Abstract method to create a ModelHandler object.
+
+        Returns:
+            ModelHandler: The created ModelHandler object.
+        """
         raise NotImplementedError
