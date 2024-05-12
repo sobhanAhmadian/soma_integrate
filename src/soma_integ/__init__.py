bedce439945557ffc67a43c26b0431aa47b40853
sobhan.ahmadian.moghadam <sobhan.ahmadian.moghadam@gmail.com>
2024-05-12 22:57:36 +0330

    Add CrossValidationResult to package init.

--- src/soma_integ/__init__.py
+++ src/soma_integ/__init__.py
@@ -4,4 +4,4 @@ from .data import Data, TrainTestSplitter, PytorchData, PytorchTrainTestSplitter
 from .evaluation import Result, evaluate_binary_classification
 from .methods import FeatureExtractor
 from .optimization import PytorchTester, PytorchTrainer
-from .optimization import Tester, Trainer, cross_validation
+from .optimization import Tester, Trainer, cross_validation, CrossValidationResult
